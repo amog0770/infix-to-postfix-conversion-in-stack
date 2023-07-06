6243b02bda8ee6d0327adb3639a82a621c603256
Amog Swami <122632753+amog0770@users.noreply.github.com>
2023-07-06 22:57:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,88 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define max 10
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define max 100
+int priority(char operator)                     //to check the priority of operator in the stack function created
+{
+    switch (operator) {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    default:
+        return -1;
+    }
+}
+int is_operator(char ch)
+{
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/'
+            || ch == '^');
+}
 
-typedef struct stack{
-    int data[max];
-    int top;
-}stack;
+char *conversion(char *infix,char *postfix)          //function for conversion of infix==>postfix
+{
+    char stack[max]; int top=-1;int i;int j=0;
+    int len=strlen(infix);                           //length of infix calculated
+   // a+b*(c^d-e)^(f+g*h)-i
+    for(i=0;i<len;i++)
+    {
+        if(isalnum(infix[i]))                        //built in function to check whether it is alphabet or digit
+        {
+            postfix[j++]=infix[i];
+        }
+        else if(infix[i]=='(')                        //direct push if it is(
+        {
+            stack[++top]=infix[i];
+        }
+        else if(infix[i]==')')                        //if it is ) then pop all element in stack till we get (
+        {
+            while (top > -1 && stack[top] != '(')
+            {postfix[j++] = stack[top--];
+            }top--;
+        }
+        else if(is_operator(infix[i]))                 //for checking priority
+        {
+            while(top>-1 && priority(stack[top])>= priority(infix[i]))
+            {
+                postfix[j++]=stack[top--];
 
-void init(stack *);
-int is_full(stack *);
-int is_empty(stack *);
-int push(stack *,int);
-int pop(stack *s);
+            }
+             stack[++top] = infix[i];
+        }
+    }
+    while(top>-1 ){                               //after completion of all element from infix then pop all element present in stack
+        postfix[j++] = stack[top--];
 
+    }
+    postfix[j]='\0';
+    return postfix;
+
+}
+//LKA/-(CB/A--(
 
 int main()
 {
-int number,k;
-stack s;
-init(&s);
-//22
-printf("\nenter the number to convert decimal to binary:");
-scanf("%d",&number);
-while(number!=0)
-{
-    k=number%2;
-    push(&s,k);
-    number=number/2;
-}
-printf("\nthe binary number after conversion is:");
-while(pop(&s)!=0)
-{
-    pop(&s);
-}
+char infix[100];char postfix[100];                           //infix taken from user ///postfix in which resukt will be stored
 
-
+    printf ("enter the infix:");
+    scanf("%s",infix);
+  char *result=conversion(infix,postfix);                     //as postfix address will be return so stored in a pointer
+    printf("conversion of infix to postfix==>%s",result);     //result printed
     return 0;
-}
-
-void init(stack *s)
-{
-    s->top=-1;
-}
-int is_empty(stack *s)
-{
-    if(s->top==-1){
-        return 0;}
-    else{
-        return 1;
-    }
-}
-int is_full(stack *s)
-{
-    if(s->top==max-1){
-        return 0;
-    }
-    else{
-        return 1;
-        }
-}
-
-int push(stack *s,int value)
-{
-    if(is_full(s)){
-        s->top=s->top+1;
-        s->data[s->top]=value;
-        printf("value pushed is:%d\n",s->data[s->top]);
-        return 1;
-    }
-    else{
-        return 0;
-    }
-
-}
-int pop(stack *s)
-{
-    if(is_empty(s))
-    {
-        printf("%d",s->data[s->top]);
-        s->top=s->top-1;
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
 }
